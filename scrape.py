9792167f88c4a6fb6a5177de2cdc02e98b46c382
Scooter C Kohler <skohler16@gmail.com>
2024-08-12 20:33:38 -0700

    made it so only stories votes 100+ points show and added pretty print

--- scrape.py
+++ scrape.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
-res = requests.get('https://news.ycombinator.com/')
+res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.titleline > a')
 subtext = soup.select('.subtext')
@@ -9,13 +10,13 @@ subtext = soup.select('.subtext')
 def create_custom_hn(links , subtext):
     hn = []
     for idx, item in enumerate(links):
-        title = links[idx].getText()
-        href = links[idx].get('href', None)
-        vote = subtext[idx].select('.scores')
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
         if len(vote):
             points = int(vote[0].getText().replace('points', ''))
-            print()
-            hn.append({'title': title, 'link': href})
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
     return hn
 
-print(create_custom_hn(links, subtext))
+pprint.pprint(create_custom_hn(links, subtext))
