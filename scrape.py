bc8dd833fa52a7ff43be24343c0ef1f4b6839a01
Scooter C Kohler <skohler16@gmail.com>
2024-08-12 20:18:50 -0700

    added a function to grab titles, links and votes

--- scrape.py
+++ scrape.py
@@ -3,4 +3,19 @@ from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/')
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup.find_all('div'))
+links = soup.select('.titleline > a')
+subtext = soup.select('.subtext')
+
+def create_custom_hn(links , subtext):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get('href', None)
+        vote = subtext[idx].select('.scores')
+        if len(vote):
+            points = int(vote[0].getText().replace('points', ''))
+            print()
+            hn.append({'title': title, 'link': href})
+    return hn
+
+print(create_custom_hn(links, subtext))
