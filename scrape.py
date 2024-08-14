5babdd227c40c01929973e0a3a5314349c7a6c76
Scooter C Kohler <skohler16@gmail.com>
2024-08-13 20:05:23 -0700

    added a page 2 to get scraped

--- scrape.py
+++ scrape.py
@@ -3,9 +3,16 @@ from bs4 import BeautifulSoup
 import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
+res2 = requests.get('https://news.ycombinator.com/?p=2')
 soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
 links = soup.select('.titleline > a')
 subtext = soup.select('.subtext')
+links2 = soup2.select('.titleline > a')
+subtext2 = soup2.select('.subtext')
+
+mega_links = links + links2
+mage_subt = subtext + subtext2
 
 def sort_by_votes(hnlist):
     return sorted(hnlist, key= lambda k:k['votes'], reverse=True)
@@ -21,4 +28,4 @@ def create_custom_hn(links , subtext):
                 hn.append({'title': title, 'link': href, 'votes': points})
     return sort_by_votes(hn)
 
-pprint.pprint(create_custom_hn(links, subtext))
+pprint.pprint(create_custom_hn(mega_links, mage_subt))
